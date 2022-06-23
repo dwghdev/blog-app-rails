d86381ed4a523fa36dc51013cfcaec6190c2fdc5
Walter <dalewaltergh@gmail.com>
2022-06-23 19:26:27 +0800

    rich text comment on posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,7 @@ class PostsController < ApplicationController
 
   def show
     @post.update views: @post.views + 1
+    @comments = @post.comments.order created_at: :desc
   end
 
   def new
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   validates :title, presence: true, length: {minimum: 5, maximum: 50}
   validates :body, presence: true
+  has_many :comments, dependent: :destroy
 end
